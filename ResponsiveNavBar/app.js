b74adf337ee9ab264c4a7369605180e1ab8a77a6
Klae the Coder <30781713+klaethecoder@users.noreply.github.com>
2021-05-19 09:14:47 -0600

    Added some changes to what changed with the toggle of the open class so that it could have a better look to the pop in menu.

--- ResponsiveNavBar/app.js
+++ ResponsiveNavBar/app.js
@@ -3,5 +3,7 @@ const navBar = document.getElementById('navBar')
 
 
 hamburgerBtn.addEventListener('click', ()=> {
+    console.log(navBar.classList.value)
+    navBar.classList.value != 'open' ? hamburgerBtn.innerHTML =  "&#10005;" :  hamburgerBtn.innerHTML ="&#9776"
     navBar.classList.toggle('open')
 })
